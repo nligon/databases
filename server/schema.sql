e85de539611241a550f1eab959fce9fbcf15d3f8
nligon <nathanaelligon@gmail.com>
2016-08-19 21:40:52 -0700

    started setup on database

--- server/schema.sql
+++ server/schema.sql
@@ -4,7 +4,19 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  objectId int NOT NULL AUTO_INCREMENT,
+  username varchar(255),
+  text varchar(255),
+  roomname varchar(255),
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (objectId)
 );
+
+/* table for rooms */
+
+/* table for users */
+
+
 
 /* Create other tables and define schemas for them here! */
 
